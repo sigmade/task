299d5766ad7d41254035af931bbf617cffcb5ad4
sigmade <sychyov1991@mail.ru>
2020-04-19 02:58:11 +0300

    Added Disqus comment
    Signed-off-by: Admin501 <Admin501@DESKTOP-HHG2HUE>

--- App/views/task/show.php
+++ App/views/task/show.php
@@ -88,6 +88,33 @@
             </div>
         </div>
         <!--/.row-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="" style="padding: 25px;">Комментарии</div>
+                <div id="disqus_thread" style="padding: 25px;"></div>
+                <script>
+
+                    /**
+                     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                    /*
+                    var disqus_config = function () {
+                    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                    };
+                    */
+                    (function () { // DON'T EDIT BELOW THIS LINE
+                        var d = document, s = d.createElement('script');
+                        s.src = 'https://task-ru.disqus.com/embed.js';
+                        s.setAttribute('data-timestamp', +new Date());
+                        (d.head || d.body).appendChild(s);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments
+                        powered by Disqus.</a></noscript>
+
+            </div>
+        </div>
     </div>
 
 </div>    <!--/.main-->
